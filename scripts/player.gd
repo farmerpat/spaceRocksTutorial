4c944f9233ced2b966975cd8e11aa0db82feb9d7
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2017-07-25 01:56:16 -0400

    thru video 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,11 @@ export var thrust = 500
 export var max_vel = 400
 export var friction = 0.65
 
+export (PackedScene) var bullet
+onready var bullet_container = get_node("bullet_container")
+
+onready var gun_timer = get_node("gun_timer")
+
 var screen_size = Vector2()
 var rot = 0
 var pos = Vector2()
@@ -19,6 +24,10 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	if Input.is_action_pressed("player_shoot"):
+		if gun_timer.get_time_left() == 0:
+			shoot()
+
 	if Input.is_action_pressed("player_left"):
 		rot += rot_speed * delta
 
@@ -45,3 +54,9 @@ func _process(delta):
 
 	set_pos(pos)
 	set_rot(rot - PI/2)
+
+func shoot():
+	gun_timer.start()
+	var b = bullet.instance()
+	bullet_container.add_child(b)
+	b.start_at(get_rot(), get_node("muzzle").get_global_pos())
